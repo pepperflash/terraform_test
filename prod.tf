f32c6bd9540aa360dc306687541a9b4443d6f72a
Alexey Shatokhin <Alexey_Shatokhin@epam.com>
2021-05-04 18:02:35 +0300

    added vpc and renamed bucket

--- prod.tf
+++ prod.tf
@@ -3,8 +3,9 @@ provider "aws" {
   region = "eu-west-3"
 }
 
-resource "aws_s3_bucket" "tf_course" {
+resource "aws_s3_bucket" "prod_tf_course" {
   bucket = "pepperflash-bucket-training"
   acl    = "private"
 }
 
+resource "aws_default_vpc" "default" {}
