56bc17b9c3c1818cc986740005f84154668f8142
Alexey Shatokhin <Alexey_Shatokhin@epam.com>
2021-05-05 17:52:00 +0300

    fixed typos and errors

--- prod.tf
+++ prod.tf
@@ -52,14 +52,14 @@ resource "aws_security_group" "prod_web" {
 
 resource "aws_instance" "prod_bastion" {
   ami           = "ami-05d3997b49b54fa92"
-  instance_type = t2.nano
+  instance_type = "t2.nano"
   
   vpc_security_group_ids = [
-  aws_security_group.web.id]
+  aws_security_group.prod_web.id]
 
    tags = {
     "Terraform" : "True"
   }
- }
 }
+
 
